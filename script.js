ca4d3dfd87c434c780b4808eacbb896c693929ab
Tales Harry <47403639+TalesHarry@users.noreply.github.com>
2023-02-01 23:02:05 -0300

    Add playRound function

--- script.js
+++ script.js
@@ -15,3 +15,46 @@ function getPlayerChoice() {
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
     return playerSelection
 }
+
+function playRound(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case "Rock":
+            switch (computerSelection){
+                case "Rock":
+                    return "It's a tie."
+
+                case "Paper":
+                    return "You lose! Paper beats Rock."
+                
+                case "Scissors":
+                    return "You win! Rock beats Scissors."
+            }
+            break;
+        
+        case "Paper":
+            switch (computerSelection){
+                case "Rock":
+                    return "You win! Paper beats Rock."
+
+                case "Paper":
+                    return "It's a tie."
+                
+                case "Scissors":
+                    return "You lose! Scissors beats Paper."
+            }
+            break;
+        
+        case "Scissors":
+            switch (computerSelection){
+                case "Rock":
+                    return "You lose! Rock beats Scissors."
+
+                case "Paper":
+                    return "You win! Scissors beats Paper."
+                
+                case "Scissors":
+                    return "It's a tie."
+            }
+            break;
+    }
+}
