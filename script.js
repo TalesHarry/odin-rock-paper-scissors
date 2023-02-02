77b18c42a0bde3edfe8eb568b78519b1238d531d
Tales Harry <47403639+TalesHarry@users.noreply.github.com>
2023-02-01 22:47:40 -0300

    Add getPlayerChoice function

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const computerOptions = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
-    position = Math.floor(Math.random() * 3);
+    let position = Math.floor(Math.random() * 3);
     return computerOptions[position]
+}
+
+function getPlayerChoice() {
+    let playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
+
+    while (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
+        playerSelection = prompt("Please enter a valid option.").toLowerCase()
+    }
+
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
+    return playerSelection
 }
